d6a767f5bafd35877a96a4626fe810f7af085070
nemasha-demel <nemashahirushidemel@gmail.com>
2024-05-09 16:12:48 +0530

    string practice 2

--- StringPractice1.java
+++ StringPractice1.java
@@ -1,25 +1,27 @@
 public class StringPractice1 {
-  public static void main(String[] args){
+  public static void main(String[] args) {
     String s1 = "hello";
     String s2 = "hello";
-    //Here output is yes. If variable values are same, only one memory address is allocated for both variables s1 and s2
-    if (s1 == s2){
+    // Here output is yes. If variable values are same, only one memory address is
+    // allocated for both variables s1 and s2
+    if (s1 == s2) {
       System.out.println("yes");
-    } else{
+    } else {
       System.out.println("no");
     }
 
-    String s1 = new String("hello");
-    String s2 = new String("hello");
-    //Here output is no.In this code two objects are created.For this objects seperate memory spaces are allocated.
-    if (s1 == s2){
+    String str1 = new String("hello");
+    String str2 = new String("hello");
+    // Here output is no.In this code two objects are created.For this objects
+    // seperate memory spaces are allocated.
+    if (str1 == str2) {
       System.out.println("yes");
-    } else{
+    } else {
       System.out.println("no");
     }
 
-    //char array to string
-    char[] charArray = {'j','a','v','a'};
+    // char array to string
+    char[] charArray = { 'j', 'a', 'v', 'a' };
     String str = new String(charArray);
     System.out.println(str);
   }
